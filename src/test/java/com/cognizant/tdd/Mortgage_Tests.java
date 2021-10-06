d0f0305b4d7c0bd72a18b50bfc911927af8bc7db
Patrick <mcbridep212@gmail.com>
2021-10-06 11:34:00 -0400

    Added deposit tests

--- src/test/java/com/cognizant/tdd/Mortgage_Tests.java
+++ src/test/java/com/cognizant/tdd/Mortgage_Tests.java
@@ -12,7 +12,7 @@ public class Mortgage_Tests {
     public void setup(){ account = new Bank_Account(1000000);}
 
     @Test
-    public void testBankAccount() {
+    public void testBankAccountWithdrawals() {
         int withdrawal = account.withdraw(500000);
         assertEquals(500000, withdrawal);
         assertEquals(500000, account.getBalance());
@@ -24,5 +24,11 @@ public class Mortgage_Tests {
         int overDraw = account.withdraw(1);
         assertEquals(0, overDraw);
         assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    public void testBankAccountDeposits() {
+        account.deposit(100000);
+        assertEquals(1100000, account.getBalance());
     }
 }

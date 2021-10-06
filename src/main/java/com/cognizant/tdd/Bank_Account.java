3898b6969858834b712ec169cddf21c5b2ed201c
Patrick <mcbridep212@gmail.com>
2021-10-06 11:28:20 -0400

    Created bank account class and some basic tests to check withdrawals and preventing overdrafts

---
+++ src/main/java/com/cognizant/tdd/Bank_Account.java
@@ -0,0 +1,30 @@
+package com.cognizant.tdd;
+
+public class Bank_Account {
+    private int balance;
+
+    public Bank_Account(int balance) {
+        this.balance = balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public int withdraw(int amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            return amount;
+        } else {
+            return 0;
+        }
+    }
+}
--- src/test/java/com/cognizant/tdd/Mortgage_Tests.java
+++ src/test/java/com/cognizant/tdd/Mortgage_Tests.java
@@ -1,4 +1,28 @@
 package com.cognizant.tdd;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class Mortgage_Tests {
+    Bank_Account account;
+
+    @BeforeEach
+    public void setup(){ account = new Bank_Account(1000000);}
+
+    @Test
+    public void testBankAccount() {
+        int withdrawal = account.withdraw(500000);
+        assertEquals(500000, withdrawal);
+        assertEquals(500000, account.getBalance());
+
+        int secondWithdrawal = account.withdraw(500000);
+        assertEquals(500000, secondWithdrawal);
+        assertEquals(0, account.getBalance());
+
+        int overDraw = account.withdraw(1);
+        assertEquals(0, overDraw);
+        assertEquals(0, account.getBalance());
+    }
 }
